32214970e47f8170ec3146f9cffe165bb97329d9
CarlosVera <veracar111@gmail.com>
2025-03-10 13:18:18 -0500

    routing with Express.js

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req,res) => {
   res.send('Hello, world');
 });
 
+app.get('/new-route', (req,res) => {
+  res.send('Hello, I am a new route');
+});
+
+app.get('/products', (req,res) => {
+  res.json({
+    name: 'product 1',
+    price: 1000
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 })
