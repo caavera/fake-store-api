4f4d2dbbe083de0be47082c7092fa66b29a2555c
CarlosVera <veracar111@gmail.com>
2025-03-11 06:46:14 -0500

    products service

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,40 +1,30 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+
+const ProductsService = require('../services/products.service');
 
 const router = express.Router();
+const service = new ProductsService();
 
 router.get('/', (req,res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      id: faker.string.uuid(),
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      imag: faker.image.url()
-    });
-  }
+  // const { size } = req.query;
+  const products = service.find();
+
   res.json(products);
 });
 
 router.get('/filter', (req,res) => {
-  const { id } = req.params;
   res.send('Hello, I am a filter');
 });
 
 router.get('/:id', (req,res) => {
   const { id } = req.params;
-  res.json({
-    id,
-    name: 'product 1',
-    price: 1000
-  });
+  const product = service.findOne(id);
+  res.json(product);
 });
 
 router.post('/', (req,res) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body
   });
