fbbc2d657ea59e562a8ce038ab007489f292bdf7
CarlosVera <veracar111@gmail.com>
2025-03-13 07:16:02 -0500

    cambios readme

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet'); // Importar helmet
+const helmet = require('helmet');
 const routerApi = require('./routes');
 
 const app = express();
